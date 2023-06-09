699efdc742b44aaf7f5d18460ee9c9b23f3b43d8
saracsilva <saracarolinasilva@gmail.com>
2023-06-09 12:11:54 +0200

    created component Navbar and installed react route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello</p>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <div className="grid grid-cols-6">
+      {" "}
+      <Link to="/" className="col-span-2 px-10  py-2  ">
+        <img src={logo} alt="" className=" w-28  " />
+      </Link>
+      <Link
+        to="/about"
+        className="col-span-4  flex flex-row-reverse items-center px-10"
+      >
+        <p className="  text-lg tex font-light"> about</p>
+      </Link>
+    </div>
+  );
+}
