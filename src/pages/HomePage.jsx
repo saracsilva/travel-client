5dd7608e9ced5f56658ca5e1500b9e9dcc29a973
saracsilva <saracarolinasilva@gmail.com>
2023-06-09 12:13:14 +0200

    created page homepage and inserted text and image

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import travelers from "../assets/travelers.svg";
 
 export default function HomePage() {
-  return <div HomePage>HomePage</div>;
+  return (
+    <div className="grid grid-cols-3  ">
+      <div className=" col-span-1">
+        <img src={travelers} alt="Two travelers" />
+      </div>
+      <div className=" col-span-2 flex items-center text-center flex-col">
+        <div className=" bg-light-pink w-4/5 flex flex-col items-center justify-center">
+          {" "}
+          <h1 className=" text-6xl font-kronaOne">Plan your next trip!</h1>
+          <p className=" font-thin">Let’s do this!</p>
+        </div>
+      </div>
+    </div>
+  );
 }
